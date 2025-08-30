a70bedfcf02962522c530dcaf790e4d1507c2cdf
juanzambranog <142639568+juanzambranog@users.noreply.github.com>
2025-08-29 20:55:13 -0500

    structure

---
+++ src/main/java/com/example/OODesign/OoDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.OODesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoDesignApplication.class, args);
+	}
+
+}
